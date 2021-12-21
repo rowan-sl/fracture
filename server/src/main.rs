c621ece369102bd5aaa5c85e44c4f54e4390485c
Rowan S-L <rowan.with.cats@gmail.com>
2021-12-21 10:35:26 -0500

    move things in main to individual functions

--- server/src/main.rs
+++ server/src/main.rs
@@ -15,13 +15,23 @@ use conf::*;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let (shutdown_tx, mut accepter_shutdown_rx): (
+    let (shutdown_tx, _): (
         Sender<ShutdownMessage>,
         Receiver<ShutdownMessage>,
     ) = channel(5);
+
     let ctrlc_transmitter = shutdown_tx.clone();
 
-    let accepter_task: task::JoinHandle<io::Result<()>> = tokio::spawn(async move {
+    let accepter_task = get_client_listener(shutdown_tx);
+    let wait_for_ctrlc = get_ctrlc_listener(ctrlc_transmitter);
+
+    let (_ctrlc_res, _accepter_res) = tokio::join!(wait_for_ctrlc, accepter_task);
+    Ok(())
+}
+
+fn get_client_listener(shutdown_tx: Sender<ShutdownMessage>) -> task::JoinHandle<io::Result<()>> {
+    tokio::spawn(async move {
+        let mut accepter_shutdown_rx = shutdown_tx.subscribe();
         // TODO make address configurable
         let listener = TcpListener::bind(ADDR).await?;
         println!(
@@ -60,9 +70,11 @@ async fn main() -> io::Result<()> {
             };
         }
         Ok(())
-    });
+    })
+}
 
-    let wait_for_ctrlc: task::JoinHandle<io::Result<()>> = tokio::spawn(async move {
+fn get_ctrlc_listener(ctrlc_transmitter: Sender<ShutdownMessage>) -> task::JoinHandle<io::Result<()>> {
+    tokio::spawn(async move {
         let sig_res = tokio::signal::ctrl_c().await;
         println!("\nRecieved ctrl+c, shutting down");
         if ctrlc_transmitter.receiver_count() == 0 {
@@ -74,8 +86,5 @@ async fn main() -> io::Result<()> {
             })
             .unwrap();
         sig_res
-    });
-
-    let (_ctrlc_res, _accepter_res) = tokio::join!(wait_for_ctrlc, accepter_task);
-    Ok(())
+    })
 }
